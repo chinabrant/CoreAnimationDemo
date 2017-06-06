6d2d4ecc290acb63f2be0658e2263488c13e3aa5
brant <sjwu1234@gmail.com>
2017-06-06 14:43:44 +0800

    Initial Commit

---
+++ CoreAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimationDemo
+//
+//  Created by brant on 2017/6/6.
+//  Copyright © 2017年 brant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreAnimationDemo
+//
+//  Created by brant on 2017/6/6.
+//  Copyright © 2017年 brant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
